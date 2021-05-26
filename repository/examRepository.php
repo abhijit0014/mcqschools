ac0157217dcf150cad0db422f88dbb38f734a5ce
Abhijit <abhijit0014@gmail.com>
2021-05-26 11:23:54 +0530

    exam save issue fix

--- repository/examRepository.php
+++ repository/examRepository.php
@@ -59,7 +59,7 @@
         {
             $creator_id = SessionManager::get("user_id");
             $count =  R::count( 'exam', "created_by = ? And created_date BETWEEN CONCAT(CURDATE(), ' ', '00:00:00') AND CONCAT(CURDATE(), ' ', '23:59:59')", [$creator_id] );
-            echo $count; exit;
+
             if($count<3){
                 return true;
             }
