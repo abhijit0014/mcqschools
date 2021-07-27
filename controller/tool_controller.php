777b9a056329063c154fb55df3d3714b2419fc9e
Abhijit <abhijit0014@gmail.com>
2021-07-27 11:48:48 +0530

    fix

---
+++ controller/tool_controller.php
@@ -0,0 +1,19 @@
+<?php
+
+    class ToolController
+    {
+
+        function __construct()
+        {
+
+        }
+
+        public function ageCalculator()
+        {
+            $view = new view('tool_age_calculator');
+            return;
+        }
+
+    }
+
+?>
