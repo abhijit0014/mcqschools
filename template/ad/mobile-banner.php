b3f9195978159075a28d2d73c281a56d39ee60f8
Abhijit <abhijit0014@gmail.com>
2021-07-28 09:00:33 +0530

    fix

---
+++ template/ad/mobile-banner.php
@@ -0,0 +1,10 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427558486793573"
+     crossorigin="anonymous"></script>
+<!-- mobile banner -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:50px"
+     data-ad-client="ca-pub-9427558486793573"
+     data-ad-slot="3283138578"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
