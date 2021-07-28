aea19f9fb93225d843d823052278839e3979b42c
Abhijit <abhijit0014@gmail.com>
2021-07-28 14:23:41 +0530

    fix

--- template/ad/horizontal.php
+++ template/ad/horizontal.php
@@ -1,12 +1,23 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427558486793573"
-     crossorigin="anonymous"></script>
-<!-- horizontal-ad -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-9427558486793573"
-     data-ad-slot="1946361395"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<style>
+.horizontal_responsive_ad { width: 320px; min-height: 100px;}
+@media(min-width: 576px) { .horizontal_responsive_ad { width: 450px; min-height: 60px; } }
+@media(min-width: 768px) { .horizontal_responsive_ad { width: 600px; min-height: 90px; } }
+@media(min-width: 992px) { .horizontal_responsive_ad { width: 900px; min-height: 90px; } }
+@media(min-width: 1200px) { .horizontal_responsive_ad { width: 1000px; min-height: 90px; } }
+</style>
+
+<div class="d-flex justify-content-center" style="background-color: #DCDCDC;">
+     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427558486793573"
+          crossorigin="anonymous"></script>
+     <!-- horizontal-ad -->
+     <ins class="adsbygoogle horizontal_responsive_ad"
+          style="display:block"
+          data-ad-client="ca-pub-9427558486793573"
+          data-ad-slot="1946361395"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+     <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+     </script>
+</div>
+
