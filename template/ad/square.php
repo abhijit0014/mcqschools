1a00a31cb77201121bf2f06372de24ca7fbafa5b
Abhijit <abhijit0014@gmail.com>
2021-07-29 10:36:58 +0530

    ad fix

---
+++ template/ad/square.php
@@ -0,0 +1,12 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427558486793573"
+     crossorigin="anonymous"></script>
+<!-- square-ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9427558486793573"
+     data-ad-slot="2653772556"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
