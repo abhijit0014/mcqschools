116de823c164c41e28ea0662367109a1c116a75f
Abhijit <abhijit0014@gmail.com>
2021-07-29 14:51:30 +0530

    ad fix

---
+++ template/ad/inFeedImgTop.php
@@ -0,0 +1,11 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427558486793573"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-6t+ed+2i-1n-4w"
+     data-ad-client="ca-pub-9427558486793573"
+     data-ad-slot="3379944139"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
