486857e0739ae0e27874f1e8dfc118a55fd0471d
Abhijit <abhijit0014@gmail.com>
2021-08-06 10:20:03 +0530

    ad fix

--- template/ad/leaderboard.php
+++ template/ad/leaderboard.php
@@ -1,8 +1,13 @@
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427558486793573"
      crossorigin="anonymous"></script>
+<style>
+.ad_responsive_style { width: 320px; height: 100px; }
+@media(min-width: 500px) { .ad_responsive_style { width: 468px; height: 60px; } }
+@media(min-width: 800px) { .ad_responsive_style { width: 728px; height: 90px; } }
+</style>     
 <!-- leaderboard728x90 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
+<ins class="adsbygoogle ad_responsive_style"
+     style="display:inline-block"
      data-ad-client="ca-pub-9427558486793573"
      data-ad-slot="3353137368"></ins>
 <script>
