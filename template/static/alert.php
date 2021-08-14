80a2271f58933e7c4461928e563db705002ce336
Abhijit <abhijit0014@gmail.com>
2021-08-14 14:53:38 +0530

    design fix

--- template/static/alert.php
+++ template/static/alert.php
@@ -44,29 +44,30 @@
 
 <!-- upcoming live exam -->
 <?php if( $GLOBALS["LIVE_EXAM_ID"]) { ?>
-<div class="mb-3">
-    <div class="row m-1 p-2 pt-4 pb-4 alert alert-info shadow-sm border border-info ">
-        <div class="col-10 col-md-7">
+<div class="mb-3 container">
+    <a href="/examcenter/live" class="text-decoration-none">
+    <div class="row p-2 pt-3 pb-3 alert alert-info shadow-sm border border-info ">
+        <div class="col-12">
             <span class="badge bg-success <?php echo date("Y-m-d H:i:s",  strtotime($liveExam['start_time'])) > date('Y-m-d H:i:s') && date("Y-m-d H:i:s",  strtotime($liveExam['end_time'])) < date('Y-m-d H:i:s') ? ' ': 'd-none' ?>">Live</span>
-            <div class="h4"><?php echo $liveExam['title'] ?></div>
-            <div class="small h6 text-secondary"><?php echo date_format(date_create($liveExam['start_time']),"l, dS F Y"); ?> </div>
-            <div class="d-flex bd-highlight">
+            <div class="h5"><?php echo $liveExam['title'] ?></div>
+        </div>
+        <div class="col-12">
+            <div class="small h6 mb-0 text-secondary"><?php echo date_format(date_create($liveExam['start_time']),"l, dS F Y"); ?> </div>
+            <div class="d-flex bd-highlight text-secondary">
                 <div class="flex-fill bd-highlight">
-                    <span class="h6 small">Starts On : <?php echo date_format(date_create($liveExam['start_time']),"h:i A"); ?></span> 
+                    <span class="small h6">STARTS ON: 
+                        <span class="text-nowrap"><?php echo date_format(date_create($liveExam['start_time']),"h:i A"); ?></span> 
+                    </span>
                 </div>
                 <div class="flex-fill bd-highlight">
-                    <span class="h6 small">Ends On : <?php echo date_format(date_create($liveExam['end_time']),"h:i A"); ?></span> 
+                    <span class="small h6">ENDS ON: 
+                        <span class="text-nowrap"> <?php echo date_format(date_create($liveExam['end_time']),"h:i A"); ?></span> 
+                    </span>
                 </div>
             </div>            
         </div>
-        <div class="col-12 col-md-4 text-end">
-            <a href="/examcenter/live" class="d-grid text-decoration-none">
-                <button class="btn border border-secondary mt-3">
-                    <?php echo date_create($liveExam['end_time']) < date_create()? 'Result': 'Details' ?>
-                </button>
-            </a>
-        </div>
     </div>
+    </a>
 </div>
 <?php } ?>
 
