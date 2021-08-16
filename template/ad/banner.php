b1c21f0b360384a6c2fa6b9c427811da3d55fa01
Abhijit <abhijit0014@gmail.com>
2021-08-16 08:13:02 +0530

    fix

--- template/ad/banner.php
+++ template/ad/banner.php
@@ -1,7 +1,11 @@
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427558486793573"
      crossorigin="anonymous"></script>
+     <style>
+.bannerAd_responsive_style { width: 300px; height: 250px; }
+@media(min-width: 800px) { .bannerAd_responsive_style { width: 728px; height: 90px; } }
+</style>      
 <!-- banner -->
-<ins class="adsbygoogle"
+<ins class="adsbygoogle bannerAd_responsive_style"
      style="display:block"
      data-ad-client="ca-pub-9427558486793573"
      data-ad-slot="3283138578"
